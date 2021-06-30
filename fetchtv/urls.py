056ba19b9f8adfea1b3c7438d81c89552f42916a
Kyle1297 <kyle_griffin97@hotmail.com>
2021-07-01 05:58:31 +1000

    renamed folder

--- fetchtv/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
-    path('api_v1/omdb/', include("omdb.urls"), name="omdb"),
-]
